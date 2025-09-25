edb1358e9316fa816203faedd2b53c01106809b4
Tommy Engström <tommy@succinct.se>
2025-09-25 10:06:54 +0200

    use ghcid insetad of ghciwatch

--- run-ghcid.sh
+++ run-ghcid.sh
@@ -15,14 +15,9 @@ echo "----------------------------------------"
 # - hpack before each startup/restart to regenerate .cabal files
 # - cabal repl with the specific test-dev component
 # - output redirected to log file
-ghciwatch \
-  --command "cabal v2-repl --enable-multi-repl " \
-  --before-reload-shell "hpack" \
-  --watch src \
-  --watch test \
-  --watch package.yaml \
-  --restart-glob "**/*.cabal" \
-  --restart-glob "package.yaml" \
-  --restart-glob "**/package.yaml" \
-  --restart-glob "cabal.project" \
-  --error-file $LOG_FILE
+ghcid \
+  --command "hpack;cabal v2-repl --enable-multi-repl lib:chatcompletion-effectful test:chatcompletion-effectful-test" \
+  --restart "cabal.project" \
+  --restart "chatcompletion-effectful.cabal" \
+  --restart "package.yaml" \
+  -o $LOG_FILE
